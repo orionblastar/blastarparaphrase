2f527a1c090b3bb747409208ba33c446dea1b517
orionblastar <orionblastar@gmail.com>
2013-09-20 14:42:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Blastar Paraphrase Data</title>
+</head>
+<body>
+Hi there, let's paraphrase some text.<p>
+<form method="post" action="paraphrase.php">
+<textarea rows="10" cols="80" name="data" wrap="physical">
+Enter the data to be converted!</textarea>:<br /> 
+<input type="submit" value="submit" name="submit"><br />
+</form><br /> 
+</body>
+</html>
